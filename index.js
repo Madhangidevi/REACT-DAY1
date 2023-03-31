c56ffe9e9966ee71bb701dab7ac15f9dd0ac089a
Madhangidevi <108646173+Madhangidevi@users.noreply.github.com>
2023-03-31 11:15:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+// import display from './components/funcop';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+    {/* <display name="Lekha"/> */}
+  </React.StrictMode>
+);
+
+
+reportWebVitals();
